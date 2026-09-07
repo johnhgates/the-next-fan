44f544bdc234abb1a8c6e14b1a66d9ea26e81288
johnhgates <jhg935@lindenwood.edu>
2026-09-07 00:12:37 -0500

    Add files via upload

---
+++ the-next-fan-render-github-2/README.md
@@ -0,0 +1,9 @@
+# Seen. Scrolled. Sold?
+
+Render/GitHub package for John Gates.
+
+Keep Render settings:
+- Root Directory: the-next-fan-render-github
+- Publish Directory: dist
+- If your Render service is a Web Service, Start Command: npx --yes serve -s dist -l $PORT
+- If it is a Static Site, no Start Command is needed.
---
+++ the-next-fan-render-github-2/dist/content.js
@@ -0,0 +1,2 @@
+const stages=[['DISCOVERY','Wait. Who is that?'],['ENGAGEMENT','Okay, one more video.'],['PARTICIPATION','I have thoughts.'],['IDENTITY','That’s my team.'],['LOYALTY','Yes, even this season.']];
+const rq="How does TikTok's personalized short-form content environment influence sports fan discovery, engagement, participation and brand loyalty?";
